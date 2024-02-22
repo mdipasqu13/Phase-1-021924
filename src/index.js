b2962217d9a35d81aecd9b90deca89d9c2657185
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-02-22 15:40:32 -0500

    has some notes, but check instructor's notes for sure

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function renderBook(book) {
 	const imgNode = document.createElement("img");
 	const deleteBtn = document.createElement("button");
 
+
 	titleNode.textContent = book.title;
 	authorNode.textContent = book.author;
 	priceNode.textContent = formatPrice(book.price);
@@ -21,6 +22,9 @@ function renderBook(book) {
 	deleteBtn.textContent = "Delete";
 	//✅ 1. on delete button click, remove card from DOM
 	//✅ 1a. attach eventListener
+  deleteBtn.addEventListener('click', () =>{
+    li.remove()
+  })
 	//✅ 1b. include callback function to remove card instance
 
 	//✅ 1c. define cb outside of renderBook
@@ -35,10 +39,31 @@ function renderBook(book) {
 
 //✅ 2. add a submit event listener to the form
 //✅ 2a. save the form node as a const
+const form = document.getElementById('book-form')
 //✅ 2b. write a testing function to fill in the values of the form
+const fillIn(form, data) {
+  form.title.value = data.title
+  form.author.value = data.author
+  form.price.value = data.price
+  form.imageUrl.value = data.imageUrl
+  form.inventory.value = form.inventory.length
+}
 //✅ 2c. invoke the fill in function
+fillIn(bookForm, bookStore.inventory[2]) //picking element at index 2 is just for example
 //✅ 2d. create the event listener 
-
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('submitted')
+  const newBook = {
+    title: bookForm.title.value,
+    author: bookForm.author.value,
+    price: bookForm.price.value,
+    reviews: [],
+    inventory: 1,
+    imageUrl: form.imageUrl.value
+  }
+  renderBook(newBook)
+})
 	//✅ 2e. create the new book and add to DOM
 
 
